c1aaa1a4e1690ec3db3788ae3e3a2fc837a52510
akashzakde <64258131+akashzakde@users.noreply.github.com>
2023-12-09 16:27:17 +0530

    Update CalculatorTest.java

--- src/test/java/com/javatechie/CalculatorTest.java
+++ src/test/java/com/javatechie/CalculatorTest.java
@@ -1,37 +1,13 @@
-public class CalculatorTest {
+package com.javatechie;
 
-    @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator(1, 2, '+');
-        double result = calculator.calculate();
-        assertEquals(3, result, 0);
-    }
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator(1, 2, '-');
-        double result = calculator.calculate();
-        assertEquals(-1, result, 0);
-    }
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
 
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator(1, 2, '*');
-        double result = calculator.calculate();
-        assertEquals(2, result, 0);
-    }
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator(1, 2, '/');
-        double result = calculator.calculate();
-        assertEquals(0.5, result, 0);
-    }
-
-    @Test
-    public void testInvalidOperator() {
-        Calculator calculator = new Calculator(1, 2, '!');
-        double result = calculator.calculate();
-        assertTrue(Double.isNaN(result));
-    }
 }
